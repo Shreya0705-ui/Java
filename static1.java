7847bf0c1d10ecffcdb447c5005ed2806926ffe0
Shreya0705-ui <gangwarshreya0705@gmail.com>
2026-01-29 12:30:57 +0530

    static cases

---
+++ static1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class static1 {
+    // static int n;
+    int n ;
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        static1 m = new static1();
+
+        // int n;
+        // n= s.nextInt();
+
+        m.n = s.nextInt();
+        System.out.println(m.n);
+
+        // System.out.println(n);
+
+    }
+    
+}
---
+++ static3.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+public class static3 {
+    static int n;
+    // int n ;
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+        // static2 m = new static2();
+
+        // int n;
+        n= s.nextInt();
+
+        // m.n = s.nextInt();
+        // System.out.println(m.n);
+
+        System.out.println(n);
+
+    }
+    
+}
+
