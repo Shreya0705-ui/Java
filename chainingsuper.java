bd6d031766ada90538212785cb68ce456bd7b99c
Shreya0705-ui <gangwarshreya0705@gmail.com>
2026-02-04 08:33:28 +0530

    Chaining of super() is not allowed

---
+++ chainingsuper.java
@@ -0,0 +1,19 @@
+class Bird {
+    Bird() {
+        System.out.println("Bird constructor");
+    }
+}
+
+class Sparrow extends Bird {
+    Sparrow() {
+        super();   
+        super();  
+        System.out.println("Sparrow constructor");
+    }
+}
+
+public class chainingsuper {
+    public static void main(String[] args) {
+        Sparrow s = new Sparrow();
+    }
+}
