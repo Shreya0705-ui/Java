aae1b2a8ad55ea68d6995a2e230c76c627f8976c
Shreya0705-ui <gangwarshreya0705@gmail.com>
2026-02-04 08:42:02 +0530

    super can not be used in chaining

---
+++ chainingsuper1.java
@@ -0,0 +1,22 @@
+public class chainingsuper1 {
+    class Bird {
+    Bird() {
+        System.out.println("Bird constructor");
+    }
+}
+
+class Sparrow extends Bird {
+    Sparrow() {
+        super();   
+        super();  
+        System.out.println("Sparrow constructor");
+    }
+}
+
+public class chainingsuper1 {
+    public static void main(String[] args) {
+        Sparrow s = new Sparrow();
+    }
+}
+
+}
