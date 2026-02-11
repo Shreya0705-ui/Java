701b5e741407f13892bf734394449a54ddda1aba
Shreya0705-ui <gangwarshreya0705@gmail.com>
2026-02-11 08:48:25 +0530

    seeing error in java while using final in diffferent way

---
+++ final1.java
@@ -0,0 +1,24 @@
+class A{
+    final int a = 10;
+    final void foo(){
+        System.out.println("Hello world");
+    }
+}
+ 
+ class B extends A{
+     a = 7;
+
+}
+
+
+public class final1 {
+    public static void main(String[] args){
+        // int a = 7;
+        A var1 = new A();
+        B var2 = new B();
+        System.out.println(var1.a);
+        System.out.println(var2.a);
+       
+
+    }
+}
